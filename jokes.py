6244896923348e85f378b30a0f3b46f084d746d1
Mark Davenport <markadavenport@gmail.com>
2021-07-03 10:27:14 -0500

    Add the files

---
+++ jokes.py
@@ -0,0 +1,20 @@
+import requests
+
+JOKE_API_URL = "https://official-joke-api.appspot.com/random_joke"
+
+
+def get_joke():
+    """Gets a random joke from the official joke API
+
+    Returns:
+        String: The joke
+    """
+    try:
+        response = requests.get(JOKE_API_URL)
+        response_json = response.json()
+        joke = response_json['setup'] + \
+            '.....' + response_json['punchline']
+        joke = joke.encode("ascii", errors="ignore").decode()
+        return joke
+    except requests.exceptions.ConnectionError:
+        raise requests.exceptions.ConnectionError
