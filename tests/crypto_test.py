73b9090f3562028a2a9369f32a0c8a06138863aa
Mark Davenport <markadavenport@gmail.com>
2021-07-04 11:49:04 -0500

    Better crypto test

--- tests/crypto_test.py
+++ tests/crypto_test.py
@@ -7,8 +7,8 @@ from crypto import TICKER_API_URL
 
 from requests.exceptions import ConnectionError
 
-def mock_json():
-    return {'btc': { 'usd': '512345'} }
+def mock_response(id, currency, value):
+    return { id: { currency: value} }
 
 
 class TestCrypt(TestCase):
@@ -17,9 +17,16 @@ class TestCrypt(TestCase):
         id = 'btc'
         vs_currency = 'usd'
         responses.add(responses.GET, f'{TICKER_API_URL}/?ids={id}&vs_currencies={vs_currency}',
-                      json=mock_json(), status=200)
+                      json=mock_response(id, vs_currency, 512345.0), status=200)
 
         self.assertEqual(get_latest_price(id, vs_currency), '$512,345')
+
+        id = 'doge'
+        vs_currency = 'usd'
+        responses.add(responses.GET, f'{TICKER_API_URL}/?ids={id}&vs_currencies={vs_currency}',
+                json=mock_response(id, vs_currency, 0.223432), status=200)
+        self.assertEqual(get_latest_price(id, vs_currency), '$0.223432')
+
 
     @responses.activate
     def test_get_latest_price_error(self):
