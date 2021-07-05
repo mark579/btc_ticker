0844c55640e16f250f38568b8032a6cd3fd591e8
Mark Davenport <markadavenport@gmail.com>
2021-07-05 17:22:45 -0500

    New formatting, sure that tests are really broke

--- crypto.py
+++ crypto.py
@@ -1,21 +1,44 @@
 import requests
 
-TICKER_API_URL = "https://api.coingecko.com/api/v3/simple/price"
+TICKER_API_URL = "https://api.coingecko.com/api/v3"
 
 
-def get_latest_price(id, currency):
-    """Gets latest price for a coin vs a currency
+class Crypto:
+    def __init__(self):
+        self.coins = Crypto.coins_request()
+    
+    def get_coin(self, id):
+        for coin in self.coins:
+            if coin['id'] == id:
+                return coin
+        raise Exception('Coin not found in coin list.')
 
-    Args:
-        id (string): ID from coingecko API /coins/list
-        currency (string): Currency from coingecko API
-                            /simple/supported_vs_currencies
+    def get_latest_price(self, ids, currency):
+        """Gets latest price for a coin vs a currency
 
-    Returns:
-        String: [The currenct price]
-    """
-    response = requests.get(
-        f'{TICKER_API_URL}/?ids={id}&vs_currencies={currency}')
-    response_json = response.json()
-    formatted = "${:,}".format(float(response_json[id][currency]))
-    return(formatted.rstrip('0').rstrip('.'))
+        Args:
+            ids (Array(string)): IDs from coingecko API /coins/list
+            currency (string): Currency from coingecko API
+                                /simple/supported_vs_currencies
+
+        Returns:
+            str: Formatted string of all prcies in format ID: Price
+            e.g. BITCOIN: $35,000 DOGECOIN: $0.2500
+        """
+        prices = ''
+        response = Crypto.price_request(ids, currency)
+        for id in ids:
+            price = "${:,}".format(float(response[id][currency]))
+            price = price.rstrip('0').rstrip('.')
+            coin = self.get_coin(id)
+            prices += f'{str.upper(coin["symbol"])}: {price} '
+
+        return(prices)
+
+    def price_request(ids, currency):
+        return requests.get(
+            f'{TICKER_API_URL}/simple/price/?ids={",".join(ids)}&vs_currencies={currency}').json()
+
+    def coins_request():
+        return requests.get(
+            f'{TICKER_API_URL}/coins/list').json()
