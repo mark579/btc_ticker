699a845882fccc9a8b13185638c284f9fab97ed1
Mark Davenport <markadavenport@gmail.com>
2021-07-27 07:58:19 -0500

    Wifi file format

--- wifi.py
+++ wifi.py
@@ -4,11 +4,8 @@ from crypto import TICKER_API_URL
 
 
 def has_internet_connection():
-
-    # Check if we can contact API
     try:
         output = subprocess.run(['nmcli', '-t', 'g'], capture_output=True)
-        print(str(output.stdout))
         if ('full' not in str(output.stdout)):
             return(False)
         urllib.request.urlopen(TICKER_API_URL + '/ping')
@@ -19,8 +16,7 @@ def has_internet_connection():
 
 
 def setup_wifi():
-    output = subprocess.run(['sudo', 'wifi-connect', '--portal-ssid', 'MAD_Crypto_Ticker'], capture_output=True)
-    print('Tried to run wifi-connect')
-    print(str(output.stdout))
+    subprocess.run(['sudo', 'wifi-connect', '--portal-ssid',
+                    'MAD_Crypto_Ticker'], capture_output=True)
     wifi = subprocess.run(['iwgetid', '-r'], capture_output=True)
     return (wifi.stdout.decode('utf-8').rstrip())
