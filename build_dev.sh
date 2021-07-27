7b7d931449fec75d91f301c1f6f6b3552fb71ecd
Mark Davenport <markadavenport@gmail.com>
2021-07-27 07:59:39 -0500

    Add build file

---
+++ build_dev.sh
@@ -0,0 +1,1 @@
+python3 -m nuitka --onefile main.py --include-data-dir=./images=images --include-data-dir=./fonts=fonts --include-package-data=luma.emulator --include-data-file=./config.yaml=config.yaml
